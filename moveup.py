87dc21b9007ff5ff8bbdbd883b4113e547e90834
Advaith Mohan <advaithmohan1999@gmail.com>
2021-10-05 04:00:33 +0530

    moveup function

--- moveup.py
+++ moveup.py
@@ -1,36 +1,34 @@
-x=[[0,0,4,0],[2,2,2,0],[2,2,2,4],[2,0,0,2]]
+def up(mat):
+	newmat = []
+	for k in range (4):
+		newmat.append([0]*4)
 
-newx = []
-for k in range (4):
-	newx.append([0]*4)
+	# to compress the grid before merging cells
+	for j in range (4):
+		pos = 0
+		for i in range (4):
+			if mat[i][j] != 0:
+				newmat[pos][j] = mat[i][j]
+				pos += 1
 
-# to compress the grid before merging cells
-for j in range (4):
-	pos = 0
+	# merging the cells
+	# take sum 
+	for j in range (4):
+		for i in range (3):
+			if newmat[i][j] == newmat[i+1][j] :
+				newmat[i][j] *= 2
+				newmat[i+1][j] = 0
+
+	for k in range (4):
+		mat[k] = [0,0,0,0]
+
+	# to compress the grid after merging cells
+	for j in range (4):
+		pos = 0
+		for i in range (4):
+			if newmat[i][j] != 0:
+				mat[pos][j] = newmat[i][j]
+				pos += 1
+
 	for i in range (4):
-		if x[i][j] != 0:
-			newx[pos][j] = x[i][j]
-			pos += 1
-
-# merging the cells
-# take sum 
-for j in range (4):
-	for i in range (3):
-		if newx[i][j] == newx[i+1][j] :
-			newx[i][j] *= 2
-			newx[i+1][j] = 0
-
-newxx= []
-for k in range (4):
-	newxx.append([0]*4)
-
-# to compress the grid after merging cells
-for j in range (4):
-	pos = 0
-	for i in range (4):
-		if newx[i][j] != 0:
-			newxx[pos][j] = newx[i][j]
-			pos += 1
-
-for i in range (4):
-	print (newxx[i])
+		print (mat[i])
